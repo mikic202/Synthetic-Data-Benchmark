e6c863717e240588fa67fb1571855635bfa31a8a
mikic202 <mikolaj.chomanski@gmail.com>
2026-02-26 11:04:42 +0100

    feat: add calculating dataset statistics

---
+++ src/metrics/quality_metrics/dataset_statistics.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from scipy.stats import kstest
+import numpy as np
+
+
+def calculate_dataset_statistics(dataset: pd.DataFrame) -> pd.DataFrame:
+    dataset_dsc = dataset.describe()
+    dataset_dsc = pd.concat(
+        [
+            dataset_dsc,
+            dataset.kurtosis().to_frame().T.set_index(pd.Index(["kurtosis"])),
+            dataset.skew().to_frame().T.set_index(pd.Index(["skew"])),
+            dataset.median().to_frame().T.set_index(pd.Index(["median"])),
+        ]
+    ).drop("count")
+
+    train_kstest = kstest(dataset, "norm")
+    dataset_dsc.loc["kstest_statistic"] = train_kstest.statistic
+    dataset_dsc.loc["kstest_pvalue"] = train_kstest.pvalue
+
+    return dataset_dsc
