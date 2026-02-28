7b496ae20dee1a756925b80261a92196e1c4552c
mikic202 <mikolaj.chomanski@gmail.com>
2026-02-28 17:56:05 +0100

    Add choosing generator class

---
+++ src/benchmark_tool/benchmark_tool.py
@@ -0,0 +1,23 @@
+from src.benchmark_tool.argparser import parse_args
+from src.model_wrappers.full_tabpfn_gen import FullTabpfnGen
+from src.model_wrappers.smote_generator import SmoteGenerator
+from src.model_wrappers.ctgan_generator import CTGANGenerator
+
+
+def get_model_class(args):
+    match args.generator_type.lower():
+        case "tabiclgen":
+            pass
+        case "smote":
+            return SmoteGenerator
+        case "ctgan":
+            return CTGANGenerator
+        case "tabpfnunsupervised":
+            return FullTabpfnGen
+        case _:
+            raise Exception("Chosen generator type is incorrect")
+
+
+def main():
+    args = parse_args()
+    generator_model_class = get_model_class(args)
