82425d097681ea586ab94f9bff85308c287720fe
mikic202 <mikolaj.chomanski@gmail.com>
2026-02-28 16:10:24 +0100

    feat: Add argument parser

---
+++ src/benchmark_tool/argparser.py
@@ -0,0 +1,22 @@
+import argparse
+from pathlib import Path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        type=Path,
+        default=Path("./benchamr_results"),
+        help="Directory where the benchmark results should be saved",
+    )
+
+    parser.add_argument(
+        "generator_type",
+        choices=["tabpfngen", "tabiclgen", "ctgan", "smote"],
+        help="Choose which generator type needs to be benchamrked",
+    )
+
+    return parser.parse_args()
