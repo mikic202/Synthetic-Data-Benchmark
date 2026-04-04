8c89679d36b8445a1709e616062df9e0a0b60a16
mikic202 <mikolaj.chomanski@gmail.com>
2026-04-04 20:30:35 +0200

    fix: fix distinction between calsification and regression tasks

--- src/metrics/difficulty_metrics/minimal_tree.py
+++ src/metrics/difficulty_metrics/minimal_tree.py
@@ -14,16 +14,15 @@ def calculate_ralation_between_dree_depth_and_accuaracy(
     real_y: list[int | float],
 ):
     accuracy_for_tree_depth = {}
-    if len(np.unique(real_y)) > NUMBER_OF_UNIQUE_ELEMENTS_FOR_CLASIFICATION or len(
-        np.unique(synthetic_y) > NUMBER_OF_UNIQUE_ELEMENTS_FOR_CLASIFICATION
+    if (
+        len(np.unique(real_y)) >= NUMBER_OF_UNIQUE_ELEMENTS_FOR_CLASIFICATION
+        or len(np.unique(synthetic_y)) >= NUMBER_OF_UNIQUE_ELEMENTS_FOR_CLASIFICATION
     ):
         tree_type = tree.DecisionTreeRegressor
     else:
         tree_type = tree.DecisionTreeClassifier
-
     for tree_depth in range(1, MAXIMUM_TREE_DEPTH + 1):
         decision_tree = tree_type(max_depth=tree_depth)
         decision_tree.fit(synthetic_x, synthetic_y)
         accuracy_for_tree_depth[tree_depth] = decision_tree.score(real_x, real_y)
-
     return accuracy_for_tree_depth
