ba26c9d11382a9bd00147eb923400b81c910c768
mikic202 <mikolaj.chomanski@gmail.com>
2026-04-04 21:09:13 +0200

    feat: add activating postprocessor based on run params

--- src/report_generator/main.py
+++ src/report_generator/main.py
@@ -6,7 +6,11 @@ from src.report_generator.postprocessors.k_anonimity_postprocesssor import (
 from src.report_generator.postprocessors.distance_to_nearest_neighbour_postprocessor import (
     DistanceToNearestNeighbourPostprocessor,
 )
+from src.report_generator.postprocessors.unlinkability_postprocessor import (
+    UnlinkabilityPostprocessor,
+)
 from src.report_generator.postprocessors.base_postprocessor import RawData
+import json
 
 
 def parse_args():
@@ -29,13 +33,30 @@ def parse_args():
     return parser.parse_args()
 
 
+def get_postprocessing_config(data_dir: Path, output_path: Path):
+    run_param_file = data_dir / "run_params.json"
+    if not run_param_file.exists():
+        raise Exception("There is no file with a run params")
+    with open(run_param_file, "r") as run_params_filehandle:
+        run_params = json.load(run_params_filehandle)
+
+    postprocessors = []
+    if run_params["k_anonimity"] == "True":
+        postprocessors.append(KAnonimityPostprocessor(output_path))
+    if run_params["distance_to_nearest"] == "True":
+        postprocessors.append(DistanceToNearestNeighbourPostprocessor(output_path))
+    if run_params["unlinkability"] == "True":
+        postprocessors.append(UnlinkabilityPostprocessor(output_path))
+
+    return postprocessors
+
+
 def main():
     args = parse_args()
     if not args.output_dir:
         args.output_dir = args.input_path / "processed"
     args.output_dir.mkdir(exist_ok=True, parents=True)
-    k_anonimity_postprocessor = KAnonimityPostprocessor(args.output_dir)
-    distance_postprocessor = DistanceToNearestNeighbourPostprocessor(args.output_dir)
+    postprocessors = get_postprocessing_config(args.input_path, args.output_dir)
     raw_data = RawData(args.input_path)
-    k_anonimity_postprocessor(raw_data)
-    distance_postprocessor(raw_data)
+    for postprocessor in postprocessors:
+        postprocessor(raw_data)
