5abd2d659dde5fba68aadc8a88c01cfd8caa50f1
mikic202 <mikolaj.chomanski@gmail.com>
2026-04-20 11:26:43 +0200

    refactor: move repeating code into single class

--- src/report_generator/aggregators/k_anonimity_aggregator.py
+++ src/report_generator/aggregators/k_anonimity_aggregator.py
@@ -1,34 +1,11 @@
 from pathlib import Path
-import glob
-from typing import Any
-import json
-import pandas as pd
+from src.report_generator.aggregators.single_value_aggregator import (
+    SingleValueAggregator,
+)
 
 
-class KAnonimityAggregator:
+class KAnonimityAggregator(SingleValueAggregator):
     def __init__(
         self, input_paths: list[Path], output_path: Path, generator_types: list[str]
     ) -> None:
-        self._k_aninimity_paths = [
-            k_aninimity_file
-            for path in input_paths
-            for k_aninimity_file in glob.glob(
-                str(path / "**/k-anonimity.json"), recursive=True
-            )
-        ]
-        self._generator_types = generator_types
-        self._output_path = output_path
-
-    def __call__(self) -> Any:
-        k_anonimity_results = {}
-        for generator_type, k_aninimity_path in zip(
-            self._generator_types, self._k_aninimity_paths
-        ):
-            with open(k_aninimity_path) as k_aninimity_file:
-                k_anonimity_data = json.load(k_aninimity_file)
-            k_anonimity_results[generator_type] = {
-                **k_anonimity_data["cl_dataset_avg"],
-                **k_anonimity_data["reg_dataset_avg"],
-            }
-        k_anonimity_dataframe = pd.DataFrame(k_anonimity_results)
-        k_anonimity_dataframe.to_latex(self._output_path / "k-anonimity.tex")
+        super().__init__(input_paths, output_path, generator_types, "k-anonimity")
---
+++ src/report_generator/aggregators/single_value_aggregator.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import glob
+from typing import Any
+import json
+import pandas as pd
+
+
+class SingleValueAggregator:
+    def __init__(
+        self,
+        input_paths: list[Path],
+        output_path: Path,
+        generator_types: list[str],
+        filename: str,
+    ) -> None:
+        self._file_paths = [
+            found_file
+            for path in input_paths
+            for found_file in glob.glob(
+                str(path / f"**/{filename}.json"), recursive=True
+            )
+        ]
+        self._generator_types = generator_types
+        self._output_path = output_path
+        self._filename = filename
+
+    def __call__(self) -> Any:
+        results = {}
+        for generator_type, k_aninimity_path in zip(
+            self._generator_types, self._file_paths
+        ):
+            with open(k_aninimity_path) as k_aninimity_file:
+                data = json.load(k_aninimity_file)
+            results[generator_type] = {
+                **data["cl_dataset_avg"],
+                **data["reg_dataset_avg"],
+            }
+        dataframe = pd.DataFrame(results)
+        dataframe.to_latex(self._output_path / f"{self._filename}.tex")
