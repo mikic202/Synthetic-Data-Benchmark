0e22e1f8dc8c3e7d3fc0f1cd272c241d96210206
mikic202 <mikolaj.chomanski@gmail.com>
2026-06-25 17:59:41 +0000

    feat: add calculating k-anonimity multiple times for one dataset

--- src/metrics/privacy_metrics/k_anonimity.py
+++ src/metrics/privacy_metrics/k_anonimity.py
@@ -3,19 +3,21 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import numpy as np
 from heapq import nsmallest
+import random
+from concurrent.futures import ProcessPoolExecutor
 
 MAX_NUMBER_OF_CLUSTERS = 20
 
 
-def calculate_k_anonimity_for_datset(
-    dataset: pd.DataFrame, identifier_atributes: list[str] | None = None
+def measure_k_anonimity_for_datset(
+    dataset: pd.DataFrame, identifier_atributes: list[str] | None = None, random_state: int = 42
 ) -> float:
     if not identifier_atributes:
         identifier_atributes = dataset.columns.tolist()
     dataset = dataset[identifier_atributes]
     smallest_ks = []
     for n_clusters in range(2, MAX_NUMBER_OF_CLUSTERS + 1):
-        clusterer = KMeans(n_clusters=n_clusters, random_state=42)
+        clusterer = KMeans(n_clusters=n_clusters, random_state=random_state)
         cluster_labels = clusterer.fit_predict(dataset)
         silhouette_avg = silhouette_score(dataset, cluster_labels)
         smallest_ks.append(
@@ -26,3 +28,12 @@ def calculate_k_anonimity_for_datset(
         nsmallest(MAX_NUMBER_OF_CLUSTERS // 4, smallest_ks, key=lambda x: -x[0]),
         key=lambda x: x[1],
     )[0][1]
+
+
+def calculate_k_anonimity_for_datset(dataset: pd.DataFrame, identifier_atributes: list[str] | None = None, number_of_repetitions: int = 5):
+    with ProcessPoolExecutor(max_workers=None) as executor:
+        results = list(executor.map(k_anonimity_process_worker, [(dataset, identifier_atributes, random.randint(0, 100)) for _ in range(number_of_repetitions)]))
+    return sum(results) / len(results)
+
+def k_anonimity_process_worker(args):
+    return measure_k_anonimity_for_datset(*args)
